c302cc0c9468d39f47fc3bb37be8623ce879e9eb
Evgeny Lapin <lev@openteam.ru>
2014-10-15 16:54:08 +0700

    rebase sender & recipient to settings

--- app/mailers/idea_mailer.rb
+++ app/mailers/idea_mailer.rb
@@ -1,8 +1,8 @@
 class IdeaMailer < ActionMailer::Base
-  default :from => 'no-reply@dzato.tomsk.ru'
+  default :from => Settings['mail.idea.sender']
 
   def new_idea_email(idea)
     @idea = idea
-    mail(:to => 'idea@dzato.tomsk.ru', :subject => 'Новое предложение для биржи идей')
+    mail(:to => Settings['mail.idea.recipient'], :subject => 'Новое предложение для биржи идей')
   end
 end
