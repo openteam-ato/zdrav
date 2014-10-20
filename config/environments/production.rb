457a97ecd265e3434b6889c466c86d2e9eb43b7e
Evgeny Lapin <lev@openteam.ru>
2014-10-20 15:06:33 +0700

    added files to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,15 @@ Zdrav::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( widget.css )
-  config.assets.precompile += %w( esp-ckeditor.js )
+  config.assets.precompile += [
+    'widget.css',
+    'esp-ckeditor.js',
+    'esp-ckeditor/custom_config.js',
+    '*.gif',
+    '*.jpeg',
+    '*.jpg',
+    '*.png'
+  ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
