5270704344cb27a2789ae6c0b624b78a701d6d1e
Evgeny Lapin <lev@openteam.ru>
2014-10-22 11:59:14 +0700

    added as_html for string

--- config/initializers/string_extentions.rb
+++ config/initializers/string_extentions.rb
@@ -2,8 +2,12 @@
 
 class String
 
+  def as_html
+    self.to_s.gsub(/&(laquo|raquo|quot);/, '"').gilensize.html_safe
+  end
+
   def slugged
-    Russian::transliterate(self).downcase.gsub(/\s+/, '-').gsub(/«|»|,/, '')
+    Russian::transliterate(self.to_s).downcase.gsub(/\s+/, '-').gsub(/«|»|,/, '')
   end
 
 end
