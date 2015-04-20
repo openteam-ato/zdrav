fb8f66f91ab4ca546ca1f63e7db2cc96c8b4a83a
Evgeny Lapin <lev@openteam.ru>
2015-04-20 10:03:38 +0600

    exclude question_2_7

--- app/models/evaluation_registry.rb
+++ app/models/evaluation_registry.rb
@@ -12,7 +12,7 @@ class EvaluationRegistry < ActiveRecord::Base
     :question_1_1, :question_1_2, :question_1_3, :question_1_4, :question_1_5, :question_1_6,
     :question_1_7, :question_1_8, :question_1_9, :question_1_10, :question_1_11,
     :question_2_1, :question_2_2, :question_2_3, :question_2_4, :question_2_5, :question_2_6,
-    :question_2_7, :question_2_8, :question_2_9,
+    :question_2_8, :question_2_9,
     :question_3_1, :question_3_2, :question_3_3, :question_3_4, :question_3_5, :question_3_6,
     :question_4_1, :question_4_2, :question_4_3, :question_5_1, :question_5_2
 
