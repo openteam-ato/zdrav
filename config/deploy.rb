1c951030f6485e9382573aaa7ec581afd93e92e6
Evgeny Lapin <lev@openteam.ru>
2015-10-01 14:58:05 +0600

    enabled whenewer/capistrano; added task for create sitemap symlinks after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,15 @@
 require 'openteam/capistrano/deploy'
 
 set :bundle_binstubs, -> { shared_path.join('bin') }
+
+namespace :sitemap do
+
+  desc 'Create symlink from shared sitemaps to public'
+  task :symlink do
+    run "ln -nfs #{shared_path}/sitemaps/sitemap.xml #{current_path}/public/sitemap.xml"
+    run "ln -nfs #{shared_path}/sitemaps/sitemap.xml.gz #{current_path}/public/sitemap.xml.gz"
+  end
+
+  after 'tagging:deploy', 'sitemap:symlink'
+
+end
