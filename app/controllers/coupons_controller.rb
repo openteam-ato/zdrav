c345ad8ca51729492b06cc875d08cb73b8f2ce31
Karmickoala904 <karmickoala904@gmail.com>
2015-10-14 12:26:23 +0600

    fix http://errors.openteam.ru/apps/53d5cfa607213a02bd01a61f/problems/561decc307213a079f39e192; http://errors.openteam.ru/apps/53d5cfa607213a02bd01a61f/problems/561b864207213a079f3937a9

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,10 +8,11 @@ class CouponsController < MainController
   def show
     number = params[:coupon][:number].gsub(/\D+/, '') if params.try(:[], :coupon).try(:[], :number).present?
     @coupon = Coupon.search { with :number, number }.results.first
-    if @coupon.created?
+    if @coupon && @coupon.created?
       coupons = Coupon.search {
         with :workflow_state, :created
         order_by :number
+        paginate :page => 1, :per_page => 1_000_000
       }.results
       @coupon_position = coupons.index(@coupon) + 1
     end
