ac8241f7aa77a2e769faef8d86bb5e14bee4d68a
Evgeniy Lapin <lev@openteam.ru>
2018-08-21 15:01:26 +0700

    added password for access to redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,9 @@
 Sidekiq::Extensions.enable_delay!
+
+Sidekiq.configure_server do |config|
+  config.redis = { password: Settings['redis.password'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { password: Settings['redis.password'] }
+end
