f5a8de83a5669d3b2f927acef422b92543b30768
the_scrubs_fan <the_scrubs_fan@mail.ru>
2018-11-13 12:10:32 +0700

    added test results to claim manage

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -6,10 +6,18 @@ class TestResult < ActiveRecord::Base
   before_save :check_right_answers
 
   def check_right_answers
-    file = YAML.load_file('data/tests/personal_control.yml')
-
-    self.right_answers = file['questions'].map.with_index do |question, index|
+    self.right_answers = test_file['questions'].map.with_index do |question, index|
       question if answers["#{index}"] == question['right'].to_s
     end.compact.count
+  end
+
+  def question_count
+    test_file['questions'].count
+  end
+
+  private
+
+  def test_file
+    YAML.load_file('data/tests/personal_control_full.yml')
   end
 end
